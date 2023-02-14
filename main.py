4ea15b005d6c9976735929adbb7204a6cd410844
Andrey <ya.belov-andreq@yandex.ru>
2023-02-14 22:10:20 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import random
 
 F = open('Слова22.TXT')  # открыть
 words = list(F) #сдалал списк
-quantity = len(words) #посчитал количество элементов в соем случае слов
-quantity = quantity - 1 # Количество слов - 1 чтитает с 0 но по количеству элементов
+quantity = len(words) #посчитал количество элементов в своем случае слов
+quantity = quantity - 1 # Количество слов - 1 считает с 0 но по количеству элементов
 V = 0 #Счетчик
 Replay = [ ] # список для повторов
-while True: # Пустой цыкл
+while True: # Пустой цикл
     a = input() # Просто ентер для продолжения
     values = random.randint(0, quantity) # Случайное число в массиве
     while values in Replay:# ищет совпадения
         values = random.randint(0, quantity) # рандомит по новой
     else: #Если повторение нет
-        Replay.append(values) # Добавляем число чтобы видеть повтарения
+        Replay.append(values) # Добавляем число чтобы видеть повторения
         V += 1 # Счетчик + 1
         WordsInMassiv = words[values] # страка с ответом еще не обрезанная
         a = str(WordsInMassiv).split('-', 2) # Обрезаю строну по -
